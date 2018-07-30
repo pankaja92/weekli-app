35d3e393547135c7799cd4b22bf3387f7dbbdad6
pankaja92 <pankajanc@gmail.com>
2018-07-30 18:26:03 +0530

    Styling started - Navbar

---
+++ src/components/Navigation/Logo.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import logo from '../../assets/logo.svg';
+
+const Logo = () => <img className="logo" src={logo} alt="logo" />;
+
+export default Logo;
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,52 +1,90 @@
 import React, { Fragment } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import SignOutButton from '../SignOut';
 import * as routes from '../../constants/routes';
+import './navigation.css';
+import Logo from './Logo';
 
 const Navigation = ({ authUser }) => (
   <div>{authUser ? <NavigationAuth /> : <NavigationNonAuth />}</div>
 );
 
 const NavigationAuth = () => (
-  <ul>
-    {/* <li><Link to={routes.LANDING}>Landing</Link></li> */}
-    <li>
-      <Link to={routes.HOME}>Home</Link>
-    </li>
-    <Reusalbe />
-    <li>
-      <Link to={routes.ACCOUNT}>Account</Link>
-    </li>
-    <li>
-      <SignOutButton />
-    </li>
-  </ul>
+  <div className="navbar">
+    <ul>
+      <div>
+        <Logo />
+      </div>
+      <div className="notlogo">
+        <li>
+          <span>
+            <NavLink to={routes.HOME} activeClassName="activeLink">
+              Home
+            </NavLink>
+          </span>
+        </li>
+        <Reusalbe />
+        <li>
+          <NavLink to={routes.ACCOUNT} activeClassName="activeLink">
+            Account
+          </NavLink>
+        </li>
+        <li>
+          <SignOutButton />
+        </li>
+      </div>
+    </ul>
+  </div>
 );
 
 const NavigationNonAuth = () => (
-  <ul>
-    <li>
-      <Link to={routes.LANDING}>Landing</Link>
-    </li>
-    <Reusalbe />
-    <li>
-      <Link to={routes.SIGN_IN}>Sign In</Link>
-    </li>
-  </ul>
+  <div className="navbar">
+    <ul>
+      <div>
+        <li>
+          <NavLink to={routes.LANDING} activeClassName="activateLogo">
+            <Logo />
+          </NavLink>
+        </li>
+      </div>
+      <div className="notlogo">
+        <Reusalbe />
+        <li>
+          <span>
+            <NavLink to={routes.SIGN_IN} activeClassName="activeLink">
+              Sign In
+            </NavLink>
+          </span>
+        </li>
+      </div>
+    </ul>
+  </div>
 );
 
 const Reusalbe = () => (
   <Fragment>
     <li>
-      <Link to={routes.FAQ}>FAQ</Link>
+      <span>
+        <NavLink to={routes.FAQ} activeClassName="activeLink">
+          FAQ
+        </NavLink>
+      </span>
     </li>
     <li>
-      <Link to={routes.EXTENSIONS}>Extension</Link>
+      <span>
+        <NavLink to={routes.EXTENSIONS} activeClassName="activeLink">
+          Extension
+        </NavLink>
+      </span>
     </li>
     <li>
-      <Link to={routes.CONTACT}>Contact</Link>
+      <span>
+        <NavLink to={routes.CONTACT} activeClassName="activeLink">
+          Contact
+        </NavLink>
+      </span>
     </li>
   </Fragment>
 );
@@ -65,4 +103,4 @@ Navigation.propTypes = {
 
 Navigation.defaultProps = { authUser: PropTypes.shape({}) };
 
-export default connect(mapStateToProps)(Navigation);
+export default withRouter(connect(mapStateToProps)(Navigation));
