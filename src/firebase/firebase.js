531604c45ef43593cfd501bbec48dfc22f4ee5d2
pankaja92 <pankajanc@gmail.com>
2018-08-04 19:41:37 +0530

    config-testing

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -14,7 +14,7 @@ const devConfig = {
 const prodConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  databaseURL: 'https://weekli-dev-12722.firebaseio.com',
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAG_EBUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
